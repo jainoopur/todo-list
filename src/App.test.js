413c69f4c29e7d049b111739f0b89ce46387861d
Noopur Jain <83579850+jainoopur@users.noreply.github.com>
2022-01-18 15:36:19 +0800

    Add basic todo list

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+// RED - GREEN - Refactor
+test('renders correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  expect(screen.getByText('todo')).toBeInTheDocument();
+  expect(screen.getByPlaceholderText('Eg. build a web app'));
+})
+
+test('allows user to add a todo list item', () => {
+  render(<App />);
+  const inputElement = screen.getByPlaceholderText('Eg. build a web app');
+  userEvent.type(inputElement, 'Build a todo list');
+  userEvent.keyboard('[Enter]');
+
+  expect(screen.getByText('Build a todo list')).toBeInTheDocument();
+})
+
+// allows user to remove an item from to-do list
